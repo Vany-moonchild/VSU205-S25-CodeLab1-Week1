efc520b3360444947208b64b6074dab1278d3297
Vany <vsu205@nyu.edu>
2025-01-23 13:01:58 -0500

    WASDController & Balloon
    Worked on the balloon and WASD controller and added a canvas to the screen.

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 public class WASDController : MonoBehaviour
 {
     public KeyCode keyUp = KeyCode.W;
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyLeft = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
 
     public Rigidbody rb;
 
     public float moveForce = 10f;
 
-    Vector3 testVec;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("START!!!");
 
-        rb = null;
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -25,7 +29,45 @@ public class WASDController : MonoBehaviour
 
         if (Input.GetKey(keyUp))
         {
-            rb.AddForce(testVec * moveForce);
+            rb.AddForce(Vector3.up * moveForce);
         }
+
+        if (Input.GetKey(keyDown))
+        {
+            rb.AddForce(Vector3.down * moveForce);
+        }
+
+        if (Input.GetKey(keyLeft))
+        {
+            rb.AddForce(Vector3.left * moveForce);
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            rb.AddForce(Vector3.right * moveForce);
+        }
+        
+        
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision enter");
+
+        if (collision.gameObject.tag == "Balloon")
+        {
+            Destroy(collision.gameObject); 
+            
+            //add score to player 2
+        }
+
+        if (collision.gameObject.tag == "Dart")
+        {
+            
+        }
+        //if collision.gameobject.tag == dart then give a point to the dart player 
+        //if the balloon hits a box on screen labeled goal then it will gain a point 
+        
+    }
+    
 }
